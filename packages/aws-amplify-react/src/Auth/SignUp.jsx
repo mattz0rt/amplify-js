4ee5d274e042ce29e8f812d7eb937ccc1ba34d94
mattz0rt <mattz0rt@gmail.com>
2018-11-26 13:41:52 -0500

    Made phone number input hidden

--- packages/aws-amplify-react/src/Auth/SignUp.jsx
+++ packages/aws-amplify-react/src/Auth/SignUp.jsx
@@ -111,7 +111,7 @@ export default class SignUp extends AuthPiece {
                             onChange={this.handleInputChange}
                         />
                     </FormField>
-                    <FormField theme={theme}>
+                    <FormField theme={theme} style={{display: 'none'}}>
                         <InputLabel>{I18n.get('Phone Number')}</InputLabel>
                         <SelectInput theme={theme}>
                             <select key="dial_code" name="dial_code" defaultValue="+1" onChange={this.handleInputChange}>
